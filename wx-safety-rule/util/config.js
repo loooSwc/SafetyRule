6a13c9c6881301204d2137d4b40cae3d6caa8ffb
caoxiangyu <357428589@qq.com>
2020-03-20 14:50:27 +0800

    commit config and css

--- wx-safety-rule/util/config.js
+++ wx-safety-rule/util/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apiPrefix: "miniprogram",
-  host: "http://localhost:8080",
+  host: "http://47.104.111.123:20000/",
 //  host: "https://wx-test.acmtc.com",
   key: "83B5E32784E311E8AD23525400B05776",
-  version: "2.0.0"
+  version: "1.0.0"
 }
